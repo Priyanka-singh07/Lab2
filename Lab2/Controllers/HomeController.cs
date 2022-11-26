5a6e5400cd07b618b233cebb3e80bc33caf34224
Priyanka-singh07 <priyankasingh071190@gmail.com>
2022-11-26 14:54:40 +0530

    Add project files.

---
+++ Lab2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task <IActionResult> Post()
+        {
+            List<PostViewModel> postcommentList = new List<PostViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/"))
+                {
+                    string apiresponse = await response.Content.ReadAsStringAsync();
+                    postcommentList = JsonConvert.DeserializeObject<List<PostViewModel>> (apiresponse);
+                }
+            }
+            return View(postcommentList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
